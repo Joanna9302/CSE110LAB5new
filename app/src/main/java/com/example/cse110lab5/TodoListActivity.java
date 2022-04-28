176b66646083408f07f9c7150e12f2fdd58a555f
Joanna Jiang <y6jiang@ucsd.edu>
2022-04-28 15:55:21 -0700

    Initial commit

---
+++ app/src/main/java/com/example/cse110lab5/TodoListActivity.java
@@ -0,0 +1,33 @@
+package com.example.cse110lab5;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
+
+public class TodoListActivity extends AppCompatActivity {
+    public RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_list);
+
+        TodoListAdapter adapter = new TodoListAdapter();
+        adapter.setHasStableIds(true);
+
+        recyclerView = findViewById(R.id.todo_items);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        adapter.setTodoListItems(TodoListItem.loadJSON(this, "demo_todos.json"));
+       //List<TodoListItem> todos = TodoListItem.loadJSON(this, "demo_todos.json");
+        // Log.d("TodoListActivity", todos.toString());
+    }
+
+}
